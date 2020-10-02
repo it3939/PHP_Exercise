38a2e111e60de0cb6dd058d28065b7e2451dc45f
GotoRen <r0719en@pluslab.org>
2020-10-02 21:03:32 +0900

    [2] 2020/10/01

--- 02-2/sample12-04.php
+++ 02-2/sample12-04.php
@@ -10,7 +10,11 @@
     ini_set('display_errors', "On");
     // 受け取った変数をrcv_で始める変数名に展開
     extract($_REQUEST, EXTR_SKIP|EXTR_PREFIX_ALL|EXTR_REFS, "rcv");
-
+    /*** flagsパラメータ
+     * EXTR_SKIP：衝突があった場合、存在する変数は上書きされない
+     * EXTR_PREFIX_ALL：全ての変数の前に prefix を付ける
+     * EXTR_REFS：変数を参照として展開する
+     ***/
     print "送信されたデータは<br />";
 
     print "名前：" . $rcv_username . "<br />";
