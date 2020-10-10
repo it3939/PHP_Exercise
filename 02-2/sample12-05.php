44fbedb771e61add09439135894d60decc75e040
GotoRen <r0719en@pluslab.org>
2020-10-10 23:31:12 +0900

    [2] 2020/10/01

--- 02-2/sample12-05.php
+++ 02-2/sample12-05.php
@@ -23,13 +23,8 @@
     <hr>
     名前と住所を入力して[送信]ボタンをクリックしてください。
     <form action="<?= $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-        <!--値が存在しない場合-->
-        <p>名前：<input size="40" type="text" name="username" value="<?= $name ?>"></p>
-        <p>住所：<input size="40" type="text" name="useraddress" value="<?= $address ?>"></p>
-
-        <!--値が存在する場合-->
-        <!--<p>名前：<input size="40" type="text" name="username" value="GotoRen"></p>-->
-        <!--<p>住所：<input size="40" type="text" name="useraddress" value="AIT"></p>-->
+        <p>名前：<input size="40" type="text" name="username" value="<?= $name='' ?>"></p>
+        <p>住所：<input size="40" type="text" name="useraddress" value="<?= $address='' ?>"></p>
         <input type="submit" name="btnExec" value="送信">
     </form>
 </body>
