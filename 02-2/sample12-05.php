eb4fc902c8cb5385abe0608a43e4884f01d946d3
GotoRen <r0719en@pluslab.org>
2020-10-11 18:24:25 +0900

    [2] 2020/10/01

--- 02-2/sample12-05.php
+++ 02-2/sample12-05.php
@@ -7,6 +7,8 @@
 <body>
 <h3>フォームデータ受け取り</h3>
 <?php
+    ini_set('display_errors', "Off"); // XAMPPではセッション変数を破棄すると未定義扱いの警告エラーが出るため非表示にする
+
     if (isset($_POST['btnExec'])) {
         // 送信ボタンがクリックされたとき
         $name = $_POST['username'];
@@ -23,8 +25,8 @@
     <hr>
     名前と住所を入力して[送信]ボタンをクリックしてください。
     <form action="<?= $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-        <p>名前：<input size="40" type="text" name="username" value="<?= $name='' ?>"></p>
-        <p>住所：<input size="40" type="text" name="useraddress" value="<?= $address='' ?>"></p>
+        <p>名前：<input size="40" type="text" name="username" value="<?= $name ?>"></p>
+        <p>住所：<input size="40" type="text" name="useraddress" value="<?= $address ?>"></p>
         <input type="submit" name="btnExec" value="送信">
     </form>
 </body>
